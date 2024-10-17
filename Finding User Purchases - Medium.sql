6f3af0d2eb9fe7311fa91b5e8dd600d5de30abb5
Youssef Amr <88115521+Youssefamr15@users.noreply.github.com>
2024-10-17 21:05:47 +0300

    Create Finding User Purchases - Medium.sql

---
+++ Finding User Purchases - Medium.sql
@@ -0,0 +1,6 @@
+SELECT DISTINCT a1.user_id
+FROM amazon_transactions a1
+JOIN amazon_transactions a2 ON a1.user_id = a2.user_id
+AND a1.id <> a2.id
+AND DATEDIFF(a2.created_at, a1.created_at) BETWEEN 0 AND 7
+ORDER BY a1.user_id
